aed76dea2c2362d2743c9cd8772fd3cda9c8e792
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-10-03 12:27:32 +0100

    Update app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
 
 function App() {
   return (
     <div className="App">
-    Zahra 
+      Zahra
+      <Home />
+      <Register />
     </div>
   );
 }
