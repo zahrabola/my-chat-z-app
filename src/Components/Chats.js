5aa623ac805fd0ac0f61d30cf737c36b73421042
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-10-03 19:38:22 +0100

    Update chats

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Chats = () => {
-  return (<div> Chats </div>)
+  return (
+    <div className="chats">
+      <div className="userchat">
+        <img
+          id="nav-img"
+          alt=""
+          src="https://images.pexels.com/photos/8601231/pexels-photo-8601231.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <div className="userchatinfo">
+          <span>chat</span>
+          <p> sms Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -7,12 +7,16 @@ const Search = () => {
         <input type="text" placeholder="Find a user" />
       </div>
       <div className="userchat">
-        <img src="" alt="" />
+  <img
+            id="nav-img"
+            alt=""
+            src="https://images.pexels.com/photos/8601231/pexels-photo-8601231.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
         <div className="userchatinfo">
           <span> Name</span>
         </div>
       </div>
-      Search
+ 
     </div>
   );
 };
