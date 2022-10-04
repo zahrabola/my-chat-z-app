c1788617325ae53eadd66907f1145cb492f3d3a8
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-10-04 10:47:32 +0100

    Update messages

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Message = () => {
-  return (<div> Navbar </div>
-)};
+  return   <div className="message">
+    <div className="messageinfo">
+      <img src alt=""/>
+      <span>Just now</span> 
+      </div>
+      <div className="messagecontent">
+        <img src alt=""/>
+      </div>
+    </div>
+};
 
 export default Message;
