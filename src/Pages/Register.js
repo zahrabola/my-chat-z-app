b7dd5be44dd09ca42a89318005b93555ff951604
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-10-05 12:47:27 +0100

    Update
    firebase storage and Register

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import Add from "../Images/Avataradd.png";
-import { /*getAuth,*/ createUserWithEmailAndPassword } from "firebase/auth";
+import { /*getAuth,*/ createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { doc, setDoc } from "firebase/firestore"; 
+import {
+  /*getStorage,*/
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+//////////////////////////////////////////
 const Register = () => {
   const [err, setErr] = useState(false);
 
@@ -17,6 +27,62 @@ const Register = () => {
     ///const auth = getAuth();
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
+
+//////////////////////////////////
+
+
+///const storage = getStorage();
+const storageRef = ref(storage, displayName);
+
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+// Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+uploadTask.on(
+  /*"state_changed",
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log("Upload is " + progress + "% done");
+    switch (snapshot.state) {
+      case "paused":
+        console.log("Upload is paused");
+        break;
+      case "running":
+        console.log("Upload is running");
+        break;
+    }
+  },*/
+  (error) => {
+    // Handle unsuccessful uploads
+    setErr(true);
+  },
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+      /*console.log("File available at", downloadURL);*/
+      await updateProfile(res.user,{
+
+        displayName,
+        photoURL:downloadURL,
+      })
+      await setDoc(doc(db, "users", res.user.uid),
+{
+    displayName,
+              email,
+              photoURL: downloadURL,
+});
+
+    });
+  }
+);
+
+
+
     } catch (err) {
       setErr(true);
     }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { getAuth } from "firebase/auth";
-
+import { getStorage} from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,5 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-///export const storage = getStorage();
-///export const db = getFirestore();
+export const storage = getStorage();
+export const db = getFirestore();
