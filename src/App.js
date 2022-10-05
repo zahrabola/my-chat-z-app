81d4b9e89980b4d2c05560252bb681670cde6b87
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-10-05 13:13:19 +0100

    Update register
    App - routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route /*Navigate*/ } from "react-router-dom";
 import "./App.css";
-///import Home from "./Pages/Home";
-///import Login from './Pages/Login';
+import Home from "./Pages/Home";
+import Login from './Pages/Login';
 import Register from './Pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      Zahra
-      home 
-      <Register />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+      <div className="App">Zahra home</div>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -26,64 +26,52 @@ const Register = () => {
 
     ///const auth = getAuth();
     try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
+          const res = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+          );
 
-//////////////////////////////////
+          //////////////////////////////////
+          //Create a unique image name
 
+          ///const storage = getStorage();
+          const storageRef = ref(storage, displayName);
 
-///const storage = getStorage();
-const storageRef = ref(storage, displayName);
+          const uploadTask = uploadBytesResumable(storageRef, file);
 
-const uploadTask = uploadBytesResumable(storageRef, file);
+      
+          uploadTask.on(
+            (error) => {
+              // Handle unsuccessful uploads
+              setErr(true);
+            },
+            () => {
+              // Handle successful uploads on complete
+              // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+              getDownloadURL(uploadTask.snapshot.ref).then(
+                async (downloadURL) => {
+                  /*console.log("File available at", downloadURL);*/
 
-// Register three observers:
-// 1. 'state_changed' observer, called any time the state changes
-// 2. Error observer, called on failure
-// 3. Completion observer, called on successful completion
-uploadTask.on(
-  /*"state_changed",
-  (snapshot) => {
-    // Observe state change events such as progress, pause, and resume
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log("Upload is " + progress + "% done");
-    switch (snapshot.state) {
-      case "paused":
-        console.log("Upload is paused");
-        break;
-      case "running":
-        console.log("Upload is running");
-        break;
-    }
-  },*/
-  (error) => {
-    // Handle unsuccessful uploads
-    setErr(true);
-  },
-  () => {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-      /*console.log("File available at", downloadURL);*/
-      await updateProfile(res.user,{
-
-        displayName,
-        photoURL:downloadURL,
-      })
-      await setDoc(doc(db, "users", res.user.uid),
-{
-    displayName,
-              email,
-              photoURL: downloadURL,
-});
-
-    });
-  }
-);
-
-
-
-    } catch (err) {
+                  ///// create user on firebase
+                  await updateProfile(res.user, {
+                    displayName,
+                    photoURL: downloadURL,
+                  });
+                  ///// create user on firebase
+                  await setDoc(doc(db, "users", res.user.uid), {
+                    uid: res.user.uid,
+                    displayName,
+                    email,
+                    photoURL: downloadURL,
+                  });
+                  
+await setDoc(doc(db, "userChats", res.user.uid), {});
+/*navigate("/")*/;
+                });
+            }
+          );
+        } catch (err) {
       setErr(true);
     }
   };
